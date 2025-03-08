a4e04da4917d365ecc0d9070e50c7891daf566bc
Ella Mae <ellalala.mc@gmail.com>
2025-03-08 17:41:42 +0800

    feat: Add protectedRoutes

--- front/src/app/routes/index.tsx
+++ front/src/app/routes/index.tsx
@@ -1,9 +1,11 @@
 import { RouteObject } from "react-router";
 import { publicRoutes } from "./publicRoutes";
 import NotFound from "./NotFound";
+import { protectedRoutes } from "./protectedRoutes";
 
 export const routes: RouteObject[] = [
   ...publicRoutes,
+  ...protectedRoutes,
   {
     path: "*", element: <NotFound />
   }
