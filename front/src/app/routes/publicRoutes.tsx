0e5889b8133b05d094f58c7f57616033a834d7dd
Ella Mae <ellalala.mc@gmail.com>
2025-03-08 18:48:12 +0800

    feat: Add new public pages

--- front/src/app/routes/publicRoutes.tsx
+++ front/src/app/routes/publicRoutes.tsx
@@ -2,6 +2,8 @@ import { RouteObject } from "react-router";
 import { Home } from "../Home";
 import LoginPage from "../student/authentication/LoginPage";
 import SignUpPage from "../student/authentication/SignUpPage";
+import MeetOurPartners from "../MeetOurPartners";
+import WarningPage from "../student/WarningPage";
 
 export const publicRoutes: RouteObject[] = [
   {
@@ -15,5 +17,13 @@ export const publicRoutes: RouteObject[] = [
   {
     path: "/sign-up",
     element: <SignUpPage />
+  },
+  {
+    path: "/meet-our-partners",
+    element: <MeetOurPartners />
+  },
+  {
+    path: "/iEnroll",
+    element: <WarningPage />
   }
 ]
