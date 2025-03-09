5b65d593e2a4de77b2cdb5041325542a62e0bf2a
jhona06 <cestinakareljhona6@gmail.com>
2025-03-09 13:55:51 +0800

    feat: added credentials page

--- front/src/app/routes/publicRoutes.tsx
+++ front/src/app/routes/publicRoutes.tsx
@@ -2,18 +2,23 @@ import { RouteObject } from "react-router";
 import { Home } from "../Home";
 import LoginPage from "../student/authentication/LoginPage";
 import SignUpPage from "../student/authentication/SignUpPage";
+import CredentialsPage from "../admin/authentication/CredentialsPage";
 
 export const publicRoutes: RouteObject[] = [
   {
     path: "/",
-    element: <Home />
+    element: <Home />,
   },
   {
     path: "/log-in",
-    element: <LoginPage />
+    element: <LoginPage />,
   },
   {
     path: "/sign-up",
-    element: <SignUpPage />
-  }
-]
+    element: <SignUpPage />,
+  },
+  {
+    path: "/admin-credentials",
+    element: <CredentialsPage />,
+  },
+];
