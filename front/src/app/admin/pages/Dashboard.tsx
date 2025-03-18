78298593bbce437bf958dd49a2cc6dc8141e9667
Ella Mae <ellalala.mc@gmail.com>
2025-03-18 13:43:27 +0800

    feat: Remove fixed inset and navbar

--- front/src/app/admin/pages/Dashboard.tsx
+++ front/src/app/admin/pages/Dashboard.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar.tsx";
 import SubNav from "../components/SubNav.tsx";
 import { getEnrollmentData } from "../../routes/dataRoutes.tsx"; 
 
@@ -17,9 +16,9 @@ const Dashboard: React.FC = () => {
     }, []);
 
     return (
-      <div className="fixed inset-0 bg-container_1 w-full h-full overflow-hidden">
-        <Navbar />
-        <div className="mt-24 flex justify-center items-center gap-10">
+      <div className="p-4 bg-container_1 w-full overflow-hidden">
+        {/* <Navbar /> */}
+        <div className="flex justify-center items-center gap-10">
           <div className="border-2 border-text-2 py-8  w-1/4 text-center shadow-md bg-background rounded-lg">
             <h2 className="text-5xl font-bold text-primary">{enrollmentData.totalEnrollments}</h2>
             <p className="text-primary font-semibold mt-2">Enrollment Total</p>
