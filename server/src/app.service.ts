c941b1ce035520e94aa172b7e020a96be5075249
Mark-cyber-lab <marknelsondelavin10@gmail.com>
2025-03-21 01:21:47 +0800

    chore: add redis

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'prisma/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma/prisma.service.js';
 import { user } from '@prisma/client';
 
 @Injectable()
