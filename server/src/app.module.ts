0ac0d3a3d81d8a0205372e66579df8f8e536231a
Mark-cyber-lab <marknelsondelavin10@gmail.com>
2025-03-20 16:22:40 +0800

    fix: remove student module in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentsModule } from './students/students.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [StudentsModule, UsersModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
