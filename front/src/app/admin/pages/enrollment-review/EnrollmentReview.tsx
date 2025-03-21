5c9fc9dfafffc4e2e800bca5578a1dcca128b171
azerndo <jt53361@gmail.com>
2025-03-21 17:14:26 +0800

    refactor: Move EnrollmentReview component to a new directory and update routing

---
+++ front/src/app/admin/pages/enrollment-review/EnrollmentReview.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+const EnrollmentReview: React.FC = () => {
+  return <Outlet />;
+};
+
+export default EnrollmentReview;
--- front/src/app/routes/admin/enrollmentReviewRoutes.tsx
+++ front/src/app/routes/admin/enrollmentReviewRoutes.tsx
@@ -1,9 +1,9 @@
 import { RouteObject } from "react-router";
-import EnrollmentReview from "../../admin/pages/EnrollmentReview";
+import EnrollmentReview from "../../admin/pages/enrollment-review/EnrollmentReview";
 
 export const enrollmentReviewRoutes: RouteObject[] = [
   {
     path: "enrollment-review",
-    element: <EnrollmentReview />
-  }
+    element: <EnrollmentReview />,
+  },
 ];
