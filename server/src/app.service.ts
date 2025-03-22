75f71183afc0682163c59a21b4f04c6defaf6d06
Mark-cyber-lab <marknelsondelavin10@gmail.com>
2025-03-22 13:29:23 +0800

    refactor: modify prisma code

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma/prisma.service.js';
-import { user } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
-  async getHello(): Promise<Pick<user, 'username' | 'user_id'>[]> {
-    return await this.prisma.user.findMany({
+  async getHello(): Promise<Prisma.userGetPayload<{ select: { username: true; user_id: true } }>[]> {
+    return this.prisma.user.findMany({
       select: {
         username: true,
         user_id: true,
