bb8b2ec7b94302a5fff127816c482a640fd592ce
Mark-cyber-lab <marknelsondelavin10@gmail.com>
2025-03-22 17:04:05 +0800

    chore: setup logger for log files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,32 @@ import { AppService } from './app.service.js';
 import { UsersModule } from './users/users.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { DatabaseModule } from './database/database.module.js';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { LoggerModule } from './logger/logger.module.js';
 
 @Module({
-  imports: [UsersModule, AuthModule, DatabaseModule],
+  // prettier-ignore
+  imports: [
+    UsersModule, 
+    AuthModule, 
+    DatabaseModule,
+    ThrottlerModule.forRoot([{
+      name: 'shortRequest',
+      ttl: 1000,
+      limit: 3,
+    }, {
+      name: 'longRequest',
+      ttl: 60000,
+      limit: 100,
+    }]),
+    LoggerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  // prettier-ignore
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class AppModule {}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Ip } from '@nestjs/common';
 import { UsersService } from './users.service.js';
+import { SkipThrottle } from '@nestjs/throttler';
+import { LoggerService } from '../logger/logger.service.js';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  private readonly logger = new LoggerService(UsersController.name);
 
+  @SkipThrottle({ default: false })
   @Get('/all')
-  async findAll() {
+  async findAll(@Ip() ip: string) {
+    this.logger.log(`Request for all Employees\t${ip}`);
     return this.usersService.findAll();
   }
 }
