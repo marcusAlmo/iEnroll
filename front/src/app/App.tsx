16fd74f51f09c9990bdaae418f5501d62d04af2b
azerndo <jt53361@gmail.com>
2025-03-22 11:05:27 +0800

    feat: wrapped the App in the EnrollmentReview Provider

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter } from "react-router"
-import AppRouter from "./router"
+import { BrowserRouter } from "react-router";
+import AppRouter from "./router";
+import { EnrollmentReviewProvider } from "./admin/context/enrollmentReviewContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
-  )
+    <EnrollmentReviewProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </EnrollmentReviewProvider>
+  );
 }
 
-export default App
+export default App;
