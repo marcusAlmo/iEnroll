65554c64a4327fc64538d11a486364a9d0475d19
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-03-24 02:58:38 +0800

    refactor: change dashboard import directory

--- front/src/app/routes/admin/dashboardRoutes.tsx
+++ front/src/app/routes/admin/dashboardRoutes.tsx
@@ -1,5 +1,5 @@
 import { RouteObject } from "react-router";
-import Dashboard from "../../admin/pages/Dashboard";
+import Dashboard from "@/app/admin/pages/dashboard/dashboardIndex";
 import EnrollmentBreakdown from "@/app/admin/pages/dashboard/enrollment-breakdown/enrollmentbreakdown";
 import EnrollmentCount from "@/app/admin/pages/dashboard/enrollment-count/enrollmentcount";
 import EnrollmentTrend from "@/app/admin/pages/dashboard/enrollment-trend/enrollmenttrend";
