301606f05a4bcec5f8eaa9308022575dbed83e00
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-03-24 02:21:05 +0800

    feat: setup enrollment breakdown page

--- front/src/app/admin/pages/dashboard/enrollment-breakdown/enrollmentbreakdown.tsx
+++ front/src/app/admin/pages/dashboard/enrollment-breakdown/enrollmentbreakdown.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function enrollmentbreakdown() {
+const EnrollmentBreakdown: React.FC = () => {
   return (
-    <div>
-      
+    <div className="flex-grow bg-white shadow-md rounded-lg p-4 border-2 border-text-2">
+        <div className="flex w-full justify-center space-x-8">
+            <p className="text-text-2 w-1/3 text-lg p-7">
+                See the percentage of students enrolled in each grade level. 
+                This helps you understand the overall makeup of our student body.
+            </p>
+            <div className="w-2/3 flex justify-center">
+            </div>
+        </div>
     </div>
-  )
-}
+    
+  );
+};
+
+export default EnrollmentBreakdown;
