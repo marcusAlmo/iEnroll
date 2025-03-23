15e20b5b091478ef92b614f5d23aa8bd4870b59a
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-03-24 03:40:49 +0800

    build: install react-router-dom

--- front/src/app/main.tsx
+++ front/src/app/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
 import { AppProvider } from './provider.tsx';
@@ -7,7 +8,9 @@ import { AppProvider } from './provider.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppProvider>
   </StrictMode>,
 )
