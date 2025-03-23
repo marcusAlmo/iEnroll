90478943ed27310fbd8797949d187dc3e28fb353
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-03-24 02:32:15 +0800

    refactor: change import names

--- front/src/app/routes/admin/dashboardRoutes.tsx
+++ front/src/app/routes/admin/dashboardRoutes.tsx
@@ -1,9 +1,9 @@
 import { RouteObject } from "react-router";
 import Dashboard from "../../admin/pages/Dashboard";
-import EnrollmentBreakdown from "../../admin/components/EnrollmentBreakdown";
-import EnrollmentCount from "../../admin/components/EnrollmentCount";
-import EnrollmentTrend from "../../admin/components/EnrollmentTrend";
-import PlanCapacity from "../../admin/components/PlanCapacity";
+import EnrollmentBreakdown from "@/app/admin/pages/dashboard/enrollment-breakdown/enrollmentbreakdown";
+import EnrollmentCount from "@/app/admin/pages/dashboard/enrollment-count/enrollmentcount";
+import EnrollmentTrend from "@/app/admin/pages/dashboard/enrollment-trend/enrollmenttrend";
+import PlanCapacity from "@/app/admin/pages/dashboard/plan-capacity/plancapacity";
 
 export const dashboardRoutes: RouteObject[] = [
   {
