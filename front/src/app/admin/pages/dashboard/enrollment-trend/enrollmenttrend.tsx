15e28c78c4b1fff16b6ce671e6784b5f1ece7fc0
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-03-24 03:15:31 +0800

    feat: import enrollmenttrend

--- front/src/app/admin/pages/dashboard/enrollment-trend/enrollmenttrend.tsx
+++ front/src/app/admin/pages/dashboard/enrollment-trend/enrollmenttrend.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import EnrollmentTrendChart from "@/app/admin/components/EnrollmentTrendChart";
 
-export default function enrollmenttrend() {
+const EnrollmentTrend: React.FC = () => {
   return (
-    <div>
-      
+    <div className="flex-grow bg-white shadow-md rounded-lg p-4 border-2 border-text-2">
+        <div className="flex w-full justify-center space-x-8">
+            <p className="text-text-2 w-1/3 text-lg p-7">
+              Track how enrollment has changed over time. This graph shows the growth or decline of student numbers, helping you identify trends.
+            </p>
+            <div className="w-2/3 flex justify-center">
+                <EnrollmentTrendChart />
+            </div>
+        </div>
     </div>
-  )
-}
+    
+  );
+};
+
+export default EnrollmentTrend;
