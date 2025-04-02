78bffa1aee929fc8b32d85d09f78f3c3b2dacf1c
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-04-02 20:04:36 +0800

    feat: added EnrollmentManagement

--- client/src/app/admin/pages/enrollment-management/EnrollmentManagement.tsx
+++ client/src/app/admin/pages/enrollment-management/EnrollmentManagement.tsx
@@ -1,9 +1,44 @@
-import { Outlet } from "react-router";
+import SubNav from "../../components/SubNav_Management";
+import { useState } from "react";
+import Announcements from "@/app/admin/pages/enrollment-management/announcements/announcement";
+import SchoolDetails from "@/app/admin/pages/enrollment-management/school-details/schooldetails";
+import SchoolClassification from "@/app/admin/pages/enrollment-management/school-classification/schoolclasification";
+import Fees from "@/app/admin/pages/enrollment-management/fees/fees";
+import GradeLevels from "@/app/admin/pages/enrollment-management/grade-levels/gradelevels";
+import EnrollmentSchedule from "@/app/admin/pages/enrollment-management/enrollment-schedule/enrollmentschedule";
+import Requirements from "@/app/admin/pages/enrollment-management/requirements/requirements";
 
-export default function EnrollmentManagement() {
-  return (
-    <div>
-      <Outlet />
+
+const EnrollmentManagement: React.FC = () => {
+  const [activeTab, setActiveTab] = useState("announcements");
+
+const renderTabContent = () => {
+    switch (activeTab) {
+      case "announcements":
+        return <Announcements/>;
+      case "school-details":
+        return <SchoolDetails/>;
+      case "school-classification":
+        return <SchoolClassification />;
+      case "fees":
+        return <Fees/>;
+      case "grade-levels":
+        return <GradeLevels/>;
+      case "enrollment-schedule":
+        return <EnrollmentSchedule/>;
+      case "requirements":
+        return <Requirements/>;
+      
+      default:
+        return <Announcements/>;
+    }
+  };
+return (
+  <div className="w-full min-h-screen bg-container-1">
+    <div>{renderTabContent()}</div>  
+      <SubNav activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
-}
+};
+
+export default EnrollmentManagement;
