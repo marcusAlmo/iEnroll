a9115bd038a9da8a6902d2aff811d443b4420771
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-04-02 20:03:21 +0800

    feat: added Subnav for Enrollment-Management

---
+++ client/src/app/admin/components/SubNav_Management.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+interface SubNavItem {
+  label: string;
+  value: string;
+}
+
+interface SubNavProps {
+  items: SubNavItem[];
+  activeItem: string;
+  setActiveItem: (item: string) => void;
+}
+
+const SubNav: React.FC<SubNavProps> = ({ items, activeItem, setActiveItem }) => {
+  return (
+    <nav className="flex gap-x-1 items-center justify-center text-center">
+      {items.map((item) => (
+        <button
+          key={item.label}
+          onClick={() => setActiveItem(item.value)}
+          className={`cursor-pointer rounded-[10px] px-4 py-2 transition ease-in-out duration-300 hover:bg-accent hover:text-background ${
+            activeItem === item.value
+              ? "bg-accent font-semibold text-background transition ease-in-out hover:bg-primary hover:text-background"
+              : "bg-background text-text-2"
+          }`}
+        >
+          {item.label}
+        </button>
+      ))}
+    </nav>
+  );
+};
+
+const SubNavIndex: React.FC<{ activeTab: string; setActiveTab: (tab: string) => void }> = ({ activeTab, setActiveTab }) => {
+  const subNavItems = [
+    { label: "Announcements", value: "announcements" },
+    { label: "School Details", value: "school-details" },
+    { label: "School Classification", value: "school-classification" },
+    { label: "Fees", value: "fees" },
+    { label: "Grade Levels", value: "grade-levels" },
+    { label: "Enrollment Schedule", value: "enrollment-schedule" },
+    { label: "Requirements", value: "requirements" },
+  ];
+
+  return (
+      <div className="flex justify-center items-center">
+        <div className="rounded-[10px] max-h-18 border border-text-2 bg-background p-2 mt-8">
+          <SubNav items={subNavItems} activeItem={activeTab} setActiveItem={setActiveTab} />
+        </div>
+      </div>
+  );
+};
+
+export default SubNavIndex;
