6df275ba8b95c80bbbd7e2451e6ca0bd5daa82e6
Gio Kent <kennethbelardo55@gmail.com>
2025-04-08 13:12:43 +0800

    refactor: Renamed sample controller return into Hrllo World

--- server2/apps/enrollment/src/enrollment.controller.ts
+++ server2/apps/enrollment/src/enrollment.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class EnrollmentController {
   @Get()
   getHello(): string {
-    return 'Hello my Nigga';
+    return 'Hello World!';
   }
 }
