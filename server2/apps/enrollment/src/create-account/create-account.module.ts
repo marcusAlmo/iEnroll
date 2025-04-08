9507bfcb99cf6ccbbf15ace0bfbea7441478e4cc
Gio Kent <kennethbelardo55@gmail.com>
2025-04-08 13:14:08 +0800

    chore: add PrismaModule to CreateAccountModule imports

--- server2/apps/enrollment/src/create-account/create-account.module.ts
+++ server2/apps/enrollment/src/create-account/create-account.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CreateAccountService } from './create-account.service';
 import { CreateAccountController } from './create-account.controller';
+import { PrismaModule } from '@libs/prisma/src/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [CreateAccountService],
   controllers: [CreateAccountController],
 })
