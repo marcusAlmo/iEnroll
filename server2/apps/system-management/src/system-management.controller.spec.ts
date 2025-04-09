a0ba283ca6b4cb9cb0b86ef333d4e312251e0606
Gio Kent <kennethbelardo55@gmail.com>
2025-04-09 11:20:48 +0800

    refactor: update import paths and remove unused services in various controllers and tests

--- server2/apps/system-management/src/system-management.controller.spec.ts
+++ server2/apps/system-management/src/system-management.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SystemManagementController } from './system-management.controller';
-import { SystemManagementService } from './system-management.service';
 
 describe('SystemManagementController', () => {
   let systemManagementController: SystemManagementController;
@@ -8,7 +7,6 @@ describe('SystemManagementController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [SystemManagementController],
-      providers: [SystemManagementService],
     }).compile();
 
     systemManagementController = app.get<SystemManagementController>(
@@ -18,7 +16,7 @@ describe('SystemManagementController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(systemManagementController.getHello()).toBe('Hello World!');
+      expect(systemManagementController.getHello()).toBe('You are gay');
     });
   });
 });
