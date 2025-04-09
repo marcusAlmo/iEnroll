86e6d037c523c34a969940846b492345b3bf7381
Gio Kent <kennethbelardo55@gmail.com>
2025-04-09 15:11:14 +0800

    feat(create-account): enhance CreateUserDto and CreateAccountService with additional fields and validation

--- server2/apps/enrollment/src/create-account/create-account.dto.ts
+++ server2/apps/enrollment/src/create-account/create-account.dto.ts
@@ -6,8 +6,10 @@ import {
   IsEnum,
   IsNotEmpty,
   IsPhoneNumber,
+  IsNumber,
+  IsDateString,
 } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 
 export class CreateUserDto {
   @IsString()
@@ -37,14 +39,33 @@ export class CreateUserDto {
   @IsNotEmpty()
   lastName: string;
 
-  @Type(() => Date)
-  @IsDate()
-  dateOfBirth: Date;
+  @IsString()
+  @IsOptional()
+  suffix?: string;
+
+  @Transform(({ value }) =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    typeof value === 'string' ? new Date(value) : value,
+  )
+  @IsDateString()
+  dateOfBirth: string | Date;
 
   @IsEnum(['M', 'F'])
-  sex: 'M' | 'F';
+  gender: 'M' | 'F';
 
-  @IsString()
+  // @IsString()
+  // @IsNotEmpty()
+  // address: string;
+
+  @IsNumber()
   @IsNotEmpty()
-  address: string;
+  schoolId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  addressId: number;
+
+  @IsNumber()
+  @IsOptional()
+  enrollerId?: number;
 }
--- server2/apps/enrollment/src/create-account/create-account.module.ts
+++ server2/apps/enrollment/src/create-account/create-account.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CreateAccountService } from './create-account.service';
 import { CreateAccountController } from './create-account.controller';
-import { PrismaModule } from '@libs/prisma/src/prisma.module';
+import { PrismaModule } from 'libs/prisma/src/prisma.module';
+import { AuthModule } from 'libs/auth/auth.module';
+import { PrismaService } from '@lib/prisma/src/prisma.service';
+import { AuthService } from '@lib/auth/auth.service';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [CreateAccountService],
+  imports: [PrismaModule, AuthModule],
+  providers: [CreateAccountService, PrismaService, AuthService],
   controllers: [CreateAccountController],
 })
 export class CreateAccountModule {}
