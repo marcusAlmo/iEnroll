071cd6d0b595ebac9bc894163afb2a895a24e99b
azerndo <jt53361@gmail.com>
2025-04-10 14:15:51 +0800

    fix: Update navigation paths in AssignedIndex component for consistency

--- client/src/app/admin/pages/enrollment-review/assigned/assignedIndex.tsx
+++ client/src/app/admin/pages/enrollment-review/assigned/assignedIndex.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from "react";
 import { useNavigate } from "react-router";
 import SubNavCopy from "../../../components/SubNavCopy";
-import EnrollmentList from "./components/enrollmentList";
+import EnrollmentList from "./pages/enrollmentList";
 
 /**
  * AssignedIndex Component
@@ -37,14 +37,14 @@ const AssignedIndex: React.FC = () => {
         label: "Denied", // Label for the "Denied" navigation item
         onClick: () => {
           setActiveItem("Denied"); // Update active state to "Denied"
-          navigate("denied"); // Navigate to the "Denied" route
+          navigate("/admin/enrollment-review/denied"); // Navigate to the "Denied" route
         },
       },
       {
         label: "Enrolled", // Label for the "Denied" navigation item
         onClick: () => {
           setActiveItem("Enrolled"); // Update active state to "Denied"
-          navigate("enrolled"); // Navigate to the "Denied" route
+          navigate("/admin/enrollment-review/enrolled"); // Navigate to the "Denied" route
         },
       },
     ],
