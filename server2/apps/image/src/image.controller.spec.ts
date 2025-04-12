791dc3de72cc9da8ac8290cc1d00c660a7fd5196
Gio Kent <kennethbelardo55@gmail.com>
2025-04-12 21:26:57 +0800

    test: Fix test

--- server2/apps/image/src/image.controller.spec.ts
+++ server2/apps/image/src/image.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
+import { OcrService } from '@lib/ocr/ocr.service';
+import { BlurryDetectorService } from '@lib/blurry-detector/blurry-detector.service';
 
 describe('ImageController', () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,7 +11,7 @@ describe('ImageController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [ImageController],
-      providers: [ImageService],
+      providers: [ImageService, OcrService, BlurryDetectorService],
     }).compile();
 
     imageController = app.get<ImageController>(ImageController);
