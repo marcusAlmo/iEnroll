8b7d38850501035f90171a7e0b5d62ac32f08b17
Gio Kent <kennethbelardo55@gmail.com>
2025-04-18 22:00:55 +0800

    refactor: Enhance file upload functionality with Swagger documentation and response DTO

---
+++ server2/libs/dtos/src/file/v1/upload-response.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+class DocumentDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  url: string;
+
+  @ApiProperty()
+  type: string;
+
+  @ApiProperty()
+  size: number;
+
+  @ApiProperty({ type: String, nullable: true })
+  createdAt: Date | null;
+
+  @ApiProperty({ type: String, nullable: true })
+  uuid: string | null;
+}
+
+export class UploadFileResponseDto {
+  @ApiProperty()
+  success: boolean;
+
+  @ApiProperty({ type: DocumentDto })
+  document: DocumentDto;
+
+  @ApiProperty({
+    type: 'object',
+    additionalProperties: true,
+    nullable: true,
+  })
+  plugins?: Record<string, any>;
+}
