3708b3390d24b72df6fbddee9570c5ddc0aa271e
Gio Kent <kennethbelardo55@gmail.com>
2025-04-22 19:55:15 +0800

    refactor(auth): remove debug logging from JwtAuthGuard handleRequest method

--- server2/libs/auth/guards/jwt-auth.guard.ts
+++ server2/libs/auth/guards/jwt-auth.guard.ts
@@ -4,7 +4,6 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err: any, user: any) {
-    console.debug('handleRequest', err, user);
     if (err || !user) {
       throw err || new UnauthorizedException('ERR_USER_NOT_AUTHORIZED');
     }
