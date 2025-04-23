cafec19a622673fd9c42bfcad6fd4669876d7969
Mark Nelson Delavin <marknelsondelavin10@gmail.com>
2025-04-23 15:26:51 +0800

    feat; prepare the grade levels api

--- server2/apps/enrollment/src/enrollment-management/enrollment-management.module.ts
+++ server2/apps/enrollment/src/enrollment-management/enrollment-management.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AnnouncementsModule } from './announcements/announcements.module';
 import { SchoolClassificationModule } from './school-classification/school-classification.module';
 import { SchoolDetailsModule } from './school-details/school-details.module';
+import { GradeLevelsModule } from './grade-levels/grade-levels.module';
+import { FeesModule } from './fees/fees.module';
 
 @Module({
-  imports: [AnnouncementsModule, SchoolClassificationModule, SchoolDetailsModule],
+  imports: [AnnouncementsModule, SchoolClassificationModule, SchoolDetailsModule, GradeLevelsModule, FeesModule],
 })
 export class EnrollmentManagementModule {}
