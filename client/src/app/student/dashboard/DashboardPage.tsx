ea6ebda93d0d8f089af687af7b2366a5c6be41c5
Ella Mae <ellalala.mc@gmail.com>
2025-04-24 15:30:15 +0800

    feat: Create Dashboard page

--- client/src/app/student/dashboard/DashboardPage.tsx
+++ client/src/app/student/dashboard/DashboardPage.tsx
@@ -1,15 +1,52 @@
 import React from "react";
 import { useScreenSize } from "@/contexts/useScreenSize";
+import { Navigate, useNavigate } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import StatusBox from "./components/StatusBox";
 
 const DashboardPage = () => {
   const { mobile } = useScreenSize();
+  const navigate = useNavigate();
+  
+  if (!mobile) return <Navigate to="/iEnroll" />;
+
+  // To do (backend):
+  // Make the string username dynamic in Line 26
+
+  // Sample code for simulation
+  // eslint-disable-next-line prefer-const
+  let enrollmentStatus = "none"; // ["enrolled", "none", "pending"];
+  const enrollmentId = 12345;
 
   return (
-    <div>
-      Dashboard
-      {mobile}
-    </div>
+    <section className="p-8 bg-container-1">
+      <div className="flex justify-end">
+        <FontAwesomeIcon 
+          icon={faPowerOff}
+          className="text-text-2"
+          style={{ fontSize: "24px" }}
+        />
+      </div>
+
+      <div className="mt-6">
+        <h1 className="text-3xl text-primary font-semibold">Uy! <span className="text-secondary">Juan</span>, enroll ka na!</h1>
+      </div>
+      
+      {/* Show this if enrollment status is enrolled */}
+      {enrollmentStatus === "enrolled" && (
+        <div>
+
+        </div>
+      )}
+
+      <StatusBox status="Pending" />
+      
+    </section>
   );
 };
 
 export default DashboardPage;
+
+
+
