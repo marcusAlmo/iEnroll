1150fa10bf7c2f3127aa7758fe5c96cdb01b2f66
Gio Kent <kennethbelardo55@gmail.com>
2025-04-25 16:36:39 +0800

    refactor: Simplify enrollment details and status endpoints by using user decorator

--- server2/apps/api-gateway/src/enrollment/dashboard/dashboard.controller.ts
+++ server2/apps/api-gateway/src/enrollment/dashboard/dashboard.controller.ts
@@ -1,25 +1,20 @@
-import {
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { JwtAuthGuard } from '@lib/auth/guards/jwt-auth.guard';
+import { User } from '@lib/decorators/user.decorator';
 
 @Controller('dashboard')
 @UseGuards(JwtAuthGuard)
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
-  @Get(':id/enrollment-details')
-  async getEnrolleeDetails(@Param('id', ParseIntPipe) studentId: number) {
+  @Get('enrollment-details')
+  async getEnrolleeDetails(@User('user_id') studentId: number) {
     return await this.dashboardService.getEnrolleeDetails({ studentId });
   }
 
-  @Get(':id/enrollment-status')
-  async getEnrolleeStatus(@Param('id', ParseIntPipe) studentId: number) {
+  @Get('enrollment-status')
+  async getEnrolleeStatus(@User('user_id') studentId: number) {
     return await this.dashboardService.getEnrollmentStatus({ studentId });
   }
 }
