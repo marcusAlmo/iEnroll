bb2f05c8d95a0854004109b9cb9eeabb0efd388c
Mark Nelson Delavin <marknelsondelavin10@gmail.com>
2025-04-25 10:40:23 +0800

    WIP: grade levels

--- server2/apps/enrollment/src/enrollment-management/grade-levels/grade-levels.service.ts
+++ server2/apps/enrollment/src/enrollment-management/grade-levels/grade-levels.service.ts
@@ -10,46 +10,33 @@ export class GradeLevelsService {
     private readonly microserviceUtility: MicroserviceUtilityService,
   ) {}
 
-  public async getGradeLevels(
-    schoolId: number,
-  ): Promise<GradeLevels['selectedGadeLevels']> {
-    const gradeLevels: GradeLevels['selectedGadeLevels'] =
-      await this.prisma.grade_level_offered.findMany({
-        where: {
+  public async getGradeLevels(schoolId: number) {
+    
+  }
+
+
+  // UTILITY FUNCTIONS
+
+  // for retrieving the grade levels
+  private async retrievedata(schoolId: number) {
+    const data = await this.prisma.grade_section_program.findMany({
+      where: {
+        grade_level_offered: {
           school_id: schoolId,
           is_active: true,
         },
-        select: {
-          grade_level: {
-            select: {
-              grade_level_code: true,
-              grade_level: true,
-            },
-          },
-          grade_section_program: {
-            select: {
-              grade_section: {
-                select: {
-                  grade_section_id: true,
-                  section_name: true,
-                  adviser: true,
-                  admission_slot: true,
-                  max_application_slot: true,
-                },
-              },
-              academic_program: {
-                select: {
-                  program_id: true,
-                  program: true,
-                },
-              },
-            },
-          },
-        },
-      });
-
-    if (!gradeLevels) return [];
-
-    return gradeLevels;
+      },
+      select: {
+        grade_section: {
+          select: {
+            grade_section_id: true,
+            section_name: true,
+            adviser: true,
+            admission_slot: true,
+            max_application_slot: true,
+          }
+        }
+      }
+    });
   }
 }
