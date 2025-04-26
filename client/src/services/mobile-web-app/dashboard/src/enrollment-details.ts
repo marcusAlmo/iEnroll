501a2e51b3233f45bd3a2d7805ceec4c6aa6f401
Gio Kent <kennethbelardo55@gmail.com>
2025-04-26 16:15:28 +0800

    refactor: commented mockup data

--- client/src/services/mobile-web-app/dashboard/src/enrollment-details.ts
+++ client/src/services/mobile-web-app/dashboard/src/enrollment-details.ts
@@ -7,6 +7,13 @@ export const getEnrolleeDetails = async () => {
     schoolId: number;
     schoolName: string;
   }>("/api/enrollment/dashboard/enrollment/details");
+  // return {
+  //   data: {
+  //     enrollmentId: 1,
+  //     schoolId: 1,
+  //     schoolName: "Bicol University",
+  //   },
+  // };
 };
 
 export const getEnrollmentStatus = async () => {
@@ -18,8 +25,25 @@ export const getEnrollmentStatus = async () => {
     isPaid: boolean;
     dueDate: Date | undefined;
   }>("/api/enrollment/dashboard/enrollment/status");
+  // return {
+  //   data: {
+  //     enrollmentStatus: "accepted",
+  //     gradeLevel: "3",
+  //     section: "A",
+  //     program: "BS Electronics Engineering",
+  //     isPaid: true,
+  //   },
+  // } as {
+  //   data: {
+  //     enrollmentStatus: application_status;
+  //     gradeLevel: string;
+  //     section: string | undefined;
+  //     program: string | undefined;
+  //     isPaid: boolean;
+  //     dueDate: Date | undefined;
+  //   };
+  // };
 };
-
 export const getDocumentsForReupload = async () => {
   return await instance.get<
     {
