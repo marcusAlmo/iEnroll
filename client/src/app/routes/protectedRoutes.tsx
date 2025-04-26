22c822c2ef1a22d0d28104b0e6c8a2a9827917ab
Ella Mae <ellalala.mc@gmail.com>
2025-04-26 14:41:05 +0800

    feat: Add dynamic routing for student interface

--- client/src/app/routes/protectedRoutes.tsx
+++ client/src/app/routes/protectedRoutes.tsx
@@ -1,18 +1,19 @@
 import { Navigate, RouteObject } from "react-router";
 import ProtectedStudentRoute from "../student/utils/ProtectedStudentRoute";
-import DashboardPage from "../student/dashboard/DashboardPage";
 import { adminRoutes } from "./admin";
 import ProtectedAdminRoute from "../admin/layouts/admin-layouts";
+import { studentRoutes } from "./student/studentRoutes";
 
 export const protectedRoutes: RouteObject[] = [
   {
-    path: "/student/dashboard",
+    path: "/student",
     element: <ProtectedStudentRoute />,
     children: [
       {
         index: true,
-        element: <DashboardPage />
-      }
+        element: <Navigate to="dashboard" replace />
+      },
+      ...studentRoutes
     ]
   },
   {
