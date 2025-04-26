c3160af5af3ee7209bb1efc1393acc80401c46ae
Mark Nelson Delavin <marknelsondelavin10@gmail.com>
2025-04-26 23:21:37 +0800

    WIP: create service for schedule retrievaks

--- server2/apps/enrollment/src/enrollment-management/enrollment-schedule/interface/enrollment-schedule.interface.ts
+++ server2/apps/enrollment/src/enrollment-management/enrollment-schedule/interface/enrollment-schedule.interface.ts
@@ -1,1 +1,42 @@
-export interface EnrollmentSchedule {}
+export interface EnrollmentSchedule {
+  gradeLevelRaw: {
+    grade_level_offered_id: number;
+    grade_level_code: string;
+    grade_level: {
+      grade_level: string;
+    };
+    enrollment_schedule: {
+      schedule_id: number;
+      application_slot: number;
+      start_datetime: Date;
+      end_datetime: Date;
+      is_paused: boolean;
+      can_choose_section: boolean;
+    }[];
+  }[];
+
+  processedGradeLevel: {
+    gradeLevelOfferedId: number;
+    gradeLevelCode: string;
+    gradeLevel: string;
+    enrollmentSchedule: {
+      scheduleId: number;
+      applicationSlot: number;
+      startDatetime: Date;
+      endDatetime: Date;
+      isPaused: boolean;
+      canChooseSection: boolean;
+    }[];
+  }[];
+
+  receivedData: {
+    gradeLevelCode: string[];
+    schedDate: {
+      startDate: Date;
+      endDate: Date;
+    }[];
+    isPaused: boolean;
+    canChooseSection: boolean;
+    
+  }
+}
