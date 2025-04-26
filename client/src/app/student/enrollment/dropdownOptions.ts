71b6a43af1c790829b51b9ffe43de9e87c1ae961
Ella Mae <ellalala.mc@gmail.com>
2025-04-26 21:18:57 +0800

    feat: Create dropdown options for enrollment form

---
+++ client/src/app/student/enrollment/dropdownOptions.ts
@@ -0,0 +1,180 @@
+export const schools = [
+  {
+    id: 1,
+    label: "School 1",
+    sublabel: "Address 1"
+  },
+  {
+    id: 2,
+    label: "School 2",
+    sublabel: "Address 2"
+  },
+  {
+    id: 3,
+    label: "School 3",
+    sublabel: "Address 3"
+  },
+  {
+    id: 4,
+    label: "School 4",
+    sublabel: "Address 4"
+  },
+  {
+    id: 5,
+    label: "School 5",
+    sublabel: "Address 5"
+  },
+  {
+    id: 6,
+    label: "School 6",
+    sublabel: "Address 6"
+  },
+  {
+    id: 7,
+    label: "School 7",
+    sublabel: "Address 7"
+  },
+  {
+    id: 8,
+    label: "School 8",
+    sublabel: "Address 8"
+  },
+  {
+    id: 9,
+    label: "School 9",
+    sublabel: "Address 9"
+  },
+  {
+    id: 10,
+    label: "School 10",
+    sublabel: "Address 10"
+  },
+  {
+    id: 11,
+    label: "School 11",
+    sublabel: "Address 11"
+  },
+  {
+    id: 12,
+    label: "School 12",
+    sublabel: "Address 12"
+  },
+  {
+    id: 13,
+    label: "School 13",
+    sublabel: "Address 13"
+  },
+  {
+    id: 14,
+    label: "School 14",
+    sublabel: "Address 14"
+  },
+  {
+    id: 15,
+    label: "School 15",
+    sublabel: "Address 15"
+  },
+  {
+    id: 16,
+    label: "School 16",
+    sublabel: "Address 16"
+  },
+  {
+    id: 17,
+    label: "School 17",
+    sublabel: "Address 17"
+  },
+  {
+    id: 18,
+    label: "School 18",
+    sublabel: "Address 18"
+  },
+  {
+    id: 19,
+    label: "School 19",
+    sublabel: "Address 19"
+  },
+  {
+    id: 20,
+    label: "School 20",
+    sublabel: "Address 20"
+  }
+];
+
+export const levels = [
+  {
+    id: "Kindergarten",
+    label: "Kindergarten"
+  },
+  {
+    id: "Preparatory",
+    label: "Preparatory"
+  },
+  {
+    id: "Elementary",
+    label: "Elementary"
+  },
+  {
+    id: "Junior High School",
+    label: "Junior High School"
+  },
+  {
+    id: "Senior High School",
+    label: "Senior High School"
+  }
+];
+
+export const gradeLevels = [
+  {
+    id: 1,
+    label: "Kindergarten"
+  },
+  {
+    id: 2,
+    label: "Grade 1"
+  },
+  {
+    id: 3,
+    label: "Grade 2"
+  },
+  {
+    id: 4,
+    label: "Grade 3"
+  },
+  {
+    id: 5,
+    label: "Grade 4"
+  },
+  {
+    id: 6,
+    label: "Grade 5"
+  },
+  {
+    id: 7,
+    label: "Grade 6"
+  },
+  {
+    id: 8,
+    label: "Grade 7"
+  },
+  {
+    id: 9,
+    label: "Grade 8"
+  },
+  {
+    id: 10,
+    label: "Grade 9"
+  },
+  {
+    id: 11,
+    label: "Grade 10"
+  },
+  {
+    id: 12,
+    label: "Grade 11"
+  },
+  {
+    id: 13,
+    label: "Grade 12"
+  }
+];
