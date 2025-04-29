bff94247691e4a771759c8ae07d615f614ce303e
marcusAlmo <mjalmojuela.dasher@gmail.com>
2025-04-29 16:59:45 +0800

    feat: added fee_type, repopulated the db, and created the most recent backup

--- db/migration/iEnroll/migrations/V008_20250429161809__Mark_June_Almojuela.sql
+++ db/migration/iEnroll/migrations/V008_20250429161809__Mark_June_Almojuela.sql
@@ -1,12 +1,14 @@
-CREATE TYPE IF NOT EXISTS access_type AS ENUM ('read', 'modify');
+CREATE TYPE enrollment.access_type_access_list AS ENUM ('read', 'modify');
+CREATE TYPE enrollment.role_type AS ENUM ('admin', 'registrar', 'student', 'parent');
 
 CREATE TABLE IF NOT EXISTS enrollment.access_list (
     user_id INT NOT NULL,
-    dashboard_access access_type NOT NULL DEFAULT 'read',
-    enrollment_review_access access_type NOT NULL DEFAULT 'read',
-    enrollment_management_access access_type NOT NULL DEFAULT 'read',
-    personnel_center_access access_type NOT NULL DEFAULT 'read',
-    system_settings_access access_type NOT NULL DEFAULT 'read',
+    role enrollment.role_type NOT NULL,
+    dashboard_access enrollment.access_type_access_list NOT NULL DEFAULT 'read',
+    enrollment_review_access enrollment.access_type_access_list NOT NULL DEFAULT 'read',
+    enrollment_management_access enrollment.access_type_access_list NOT NULL DEFAULT 'read',
+    personnel_center_access enrollment.access_type_access_list NOT NULL DEFAULT 'read',
+    system_settings_access enrollment.access_type_access_list NOT NULL DEFAULT 'read',
     update_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT pk_access_list PRIMARY KEY (user_id)
@@ -17,7 +19,7 @@ BEGIN
     -- Bulk insert for different roles with their respective access levels
     INSERT INTO enrollment.access_list (
         user_id, 
-        role_code, 
+        role, 
         dashboard_access, 
         enrollment_review_access, 
         enrollment_management_access, 
@@ -31,31 +33,31 @@ BEGIN
             WHEN 'REG' THEN 'registrar'
             WHEN 'STU' THEN 'student'
             WHEN 'PAR' THEN 'parent'
-        END AS role_code,
+        END::enrollment.role_type AS role,
         CASE role_code
             WHEN 'ADM' THEN 'modify'
             ELSE 'read'
-        END AS dashboard_access,
-        CASE role_code
-            WHEN 'ADM' THEN 'modify'
-            WHEN 'REG' THEN 'modify'
-            ELSE 'read'
-        END AS enrollment_review_access,
+        END::enrollment.access_type_access_list AS dashboard_access,
         CASE role_code
             WHEN 'ADM' THEN 'modify'
             WHEN 'REG' THEN 'modify'
             ELSE 'read'
-        END AS enrollment_management_access,
+        END::enrollment.access_type_access_list AS enrollment_review_access,
         CASE role_code
             WHEN 'ADM' THEN 'modify'
             WHEN 'REG' THEN 'modify'
             ELSE 'read'
-        END AS personnel_center_access,
+        END::enrollment.access_type_access_list AS enrollment_management_access,
+        CASE role_code
+            WHEN 'ADM' THEN 'modify'
+            WHEN 'REG' THEN 'modify'
+            ELSE 'read'
+        END::enrollment.access_type_access_list AS personnel_center_access,
         CASE role_code
             WHEN 'ADM' THEN 'modify'
             ELSE 'read'
-        END AS system_settings_access
-    FROM enrollment.user
+        END::enrollment.access_type_access_list AS system_settings_access
+    FROM enrollment.user_role
     WHERE role_code IN ('ADM', 'REG', 'STU', 'PAR')
     ON CONFLICT (user_id) DO NOTHING;
 END $$;
