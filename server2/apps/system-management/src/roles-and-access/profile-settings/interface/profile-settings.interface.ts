a32883c6006ad2de1a7793ca89a89ca2be3b8b93
Mark Nelson Delavin <marknelsondelavin10@gmail.com>
2025-04-29 15:55:34 +0800

    WIP(roles and access): employee list

--- server2/apps/system-management/src/roles-and-access/profile-settings/interface/profile-settings.interface.ts
+++ server2/apps/system-management/src/roles-and-access/profile-settings/interface/profile-settings.interface.ts
@@ -1,1 +1,40 @@
-export interface ProfileSettings {}
+export interface ProfileSettings {
+  employeeList: {
+    name: string;
+    role: string;
+  };
+  employeeInfo: {
+    profileSettings: {
+      personalInformation: {
+        fName: string;
+        mName: string;
+        lName: string;
+        suffix: string;
+        birthDate: Date;
+        gender: string;
+      };
+      contactInformation: {
+        email: string;
+        phone: string;
+      };
+      address: {
+        streetAddress: string;
+        street: string;
+        district: string;
+        municipality: string;
+        province: string;
+      };
+    };
+    accountSettings: {
+      username: string;
+      email: string;
+    };
+    roleManagement: {
+      accessList: {
+        accessListName: string;
+        accessListPermission: string;
+      }[];
+      roleList: string;
+    };
+  };
+}
--- server2/apps/system-management/src/roles-and-access/profile-settings/profile-settings.module.ts
+++ server2/apps/system-management/src/roles-and-access/profile-settings/profile-settings.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProfileSettingsService } from './profile-settings.service';
 import { ProfileSettingsController } from './profile-settings.controller';
+import { PrismaService } from '@lib/prisma/src/prisma.service';
+import { MicroserviceUtilityService } from '@lib/microservice-utility/microservice-utility.service';
 
 @Module({
-  providers: [ProfileSettingsService],
-  controllers: [ProfileSettingsController]
+  providers: [
+    ProfileSettingsService,
+    PrismaService,
+    MicroserviceUtilityService,
+  ],
+  controllers: [ProfileSettingsController],
 })
 export class ProfileSettingsModule {}
