de05b91578745e42798f5e4a95420dee1a4048d6
Gio Kent <kennethbelardo55@gmail.com>
2025-04-30 20:40:26 +0800

    feat: Implement denied enrollment retrieval microservice

--- server2/apps/enrollment/src/review/denied/denied.controller.ts
+++ server2/apps/enrollment/src/review/denied/denied.controller.ts
@@ -1,7 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { DeniedService } from './denied.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class DeniedController {
   constructor(private readonly deniedService: DeniedService) {}
+
+  @MessagePattern({
+    cmd: 'enrollment_review_denied:get_denied_enrollments_by_school',
+  })
+  async getDeniedEnrollmentsBySchool(schoolId: number) {
+    return this.deniedService.getDeniedEnrollmentsBySchool(schoolId);
+  }
 }
