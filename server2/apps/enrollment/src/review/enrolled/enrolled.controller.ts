7f99026b2b5b641224061a7626ca752d01753c16
Gio Kent <kennethbelardo55@gmail.com>
2025-04-30 21:52:17 +0800

    feat: Enrolled microservice

--- server2/apps/enrollment/src/review/enrolled/enrolled.controller.ts
+++ server2/apps/enrollment/src/review/enrolled/enrolled.controller.ts
@@ -1,7 +1,37 @@
 import { Controller } from '@nestjs/common';
 import { EnrolledService } from './enrolled.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class EnrolledController {
   constructor(private readonly enrolledService: EnrolledService) {}
+
+  @MessagePattern({
+    cmd: 'enrollment_review_enrolled:get_all_grade_levels_by_school',
+  })
+  async getAllGradeLevelsBySchool(@Payload() payload: { schoolId: number }) {
+    return this.enrolledService.getAllGradeLevelsBySchool(payload.schoolId);
+  }
+
+  @MessagePattern({
+    cmd: 'enrollment_review_enrolled:get_all_sections_by_grade_level',
+  })
+  async getAllSectionsByGradeLevel(
+    @Payload() payload: { gradeLevelId: number },
+  ) {
+    return this.enrolledService.getAllSectionsByGradeLevel(
+      payload.gradeLevelId,
+    );
+  }
+
+  @MessagePattern({
+    cmd: 'enrollment_review_enrolled:get_all_enrolled_students_enrolled_by_section',
+  })
+  async getAllStudentsEnrolledBySection(
+    @Payload() payload: { sectionId: number },
+  ) {
+    return this.enrolledService.getAllStudentsEnrolledBySection(
+      payload.sectionId,
+    );
+  }
 }
--- server2/apps/enrollment/src/review/enrolled/enrolled.service.ts
+++ server2/apps/enrollment/src/review/enrolled/enrolled.service.ts
@@ -1,4 +1,125 @@
+import { PrismaService } from '@lib/prisma/src/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EnrolledService {}
+export class EnrolledService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllGradeLevelsBySchool(schoolId: number) {
+    const result = await this.prisma.grade_level_offered.findMany({
+      where: {
+        is_active: true,
+        school_id: schoolId,
+      },
+      select: {
+        grade_level_offered_id: true,
+        grade_level: {
+          select: {
+            grade_level: true,
+          },
+        },
+      },
+    });
+
+    return result.map((data) => ({
+      gradeId: data.grade_level_offered_id,
+      gradeName: data.grade_level.grade_level,
+    }));
+  }
+  async getAllSectionsByGradeLevel(gradeLevelId: number) {
+    const result = await this.prisma.grade_section.findMany({
+      where: {
+        grade_section_program: {
+          grade_level_offered_id: gradeLevelId,
+        },
+      },
+      select: {
+        grade_section_id: true,
+        section_name: true,
+      },
+    });
+
+    return result.map((data) => ({
+      sectionId: data.grade_section_id,
+      sectionName: data.section_name,
+    }));
+  }
+
+  // name
+  // id
+  // grade level
+  // section
+  // status
+  // enrollment date
+  async getAllStudentsEnrolledBySection(sectionId: number) {
+    const result = await this.prisma.student_enrollment.findMany({
+      where: {
+        grade_section_id: sectionId,
+      },
+      select: {
+        enrollment_datetime: true,
+        grade_section: {
+          select: {
+            section_name: true,
+            grade_section_program: {
+              select: {
+                grade_level_offered: {
+                  select: {
+                    grade_level: {
+                      select: {
+                        grade_level: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        enrollment_application: {
+          select: {
+            status: true,
+            student: {
+              select: {
+                // since enroller is the student itself
+                user_student_enroller_idTouser: {
+                  select: {
+                    first_name: true,
+                    last_name: true,
+                    middle_name: true,
+                    suffix: true,
+                    user_id: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return result.map((data) => ({
+      studentId:
+        data.enrollment_application.student.user_student_enroller_idTouser
+          .user_id,
+      firstName:
+        data.enrollment_application.student.user_student_enroller_idTouser
+          .first_name,
+      lastName:
+        data.enrollment_application.student.user_student_enroller_idTouser
+          .last_name,
+      middleName:
+        data.enrollment_application.student.user_student_enroller_idTouser
+          .middle_name,
+      suffix:
+        data.enrollment_application.student.user_student_enroller_idTouser
+          .suffix,
+      enrollmentStatus: data.enrollment_application.status,
+      enrollmentDate: data.enrollment_datetime,
+      gradeLevel:
+        data.grade_section.grade_section_program.grade_level_offered.grade_level
+          .grade_level,
+      sectionName: data.grade_section.section_name,
+    }));
+  }
+}
