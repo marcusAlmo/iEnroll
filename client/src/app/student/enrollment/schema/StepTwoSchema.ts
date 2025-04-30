25036dc9f69a624fd9c99573e0ebfce2ad22c896
Ella Mae <ellalala.mc@gmail.com>
2025-04-30 21:46:35 +0800

    feat: Add tooltips

---
+++ client/src/app/student/enrollment/schema/StepTwoSchema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const stepTwoSchema = z.object({
+  fatherFN: z.string().min(1, {
+    message: "Please enter first name of student's father"
+  }),
+  fatherMN: z.string().optional(),
+  fatherLN: z.string().min(1, {
+    message: "Please enter last name of student's father"
+  }),
+  maidenMotherFN: z.string().min(1, {
+    message: "Please enter first name of student's mother"
+  }),
+  maidenMotherMN: z.string().optional(),
+  maidenMotherLN: z.string().min(1, {
+    message: "Please enter maiden last name of student's mother"
+  }),
+  paymentMethodName: z.string().min(1, {
+    message: "Please enter a payment method"
+  }),
+  isAgree: z.literal(true, {
+    message: "Please agree to the terms and conditions before submitting"
+  })
+});
