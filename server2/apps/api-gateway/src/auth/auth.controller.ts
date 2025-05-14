1a6e9d9f1875d8bcd0e5ef66c976cfc098b1120a
Gio Kent <kennethbelardo55@gmail.com>
2025-05-14 22:01:47 +0800

    refactor: enhance authentication flow with JWT guard and cookie management

--- server2/apps/api-gateway/src/auth/auth.controller.ts
+++ server2/apps/api-gateway/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from '@lib/dtos/src/auth/v1/login.dto';
 import { Response } from 'express';
+import { JwtAuthGuard } from '@lib/auth/guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,10 +17,27 @@ export class AuthController {
 
     res.cookie('access_token', access_token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
     });
 
     return { message: 'Login successful' };
+  }
+
+  @Get('validate')
+  @UseGuards(JwtAuthGuard)
+  async validateIfAuthenticated() {
+    return { success: true };
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // must match login cookie config
+      sameSite: 'strict',
+    });
+
+    return { message: 'Logout successful' };
   }
 }
